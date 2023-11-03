bfadfe64681bd625eaf14db85e55ba7062b5cde5
clayton-schneider <schneiderc2016@gmail.com>
2023-11-03 10:05:20 -0400

    switched fonts

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,25 @@
 /** @type {import('tailwindcss').Config} */
+
+const FONT_FAMILY_BASE = [
+	'system-ui',
+	'-apple-system',
+	'BlinkMacSystemFont',
+	'Segoe UI',
+	'Roboto',
+	'Oxygen',
+	'Ubuntu',
+	'Cantarell',
+	'Open Sans',
+	'Helvetica Neue',
+	'sans-serif'
+]
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		fontFamily: {
+			sans: ['Roboto', ...FONT_FAMILY_BASE]
+		},
 		extend: {
 			padding: {
 				edge: '2.5%'
